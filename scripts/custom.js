cf3bdb6d2eb981b8e6af0845024c4191dbf95c47
Yana <yaplas.zhe@gmail.com>
2021-03-03 09:25:21 +0300

    slider for ulitka bottom

--- scripts/custom.js
+++ scripts/custom.js
@@ -493,6 +493,28 @@ minus.on('click', function () {
     }
 });
 
+
+$('.dop-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
 /*$('#plus').on('click', function () {
     let quant = +$(this).siblings($('.count')).val();
     console.log(quant);
